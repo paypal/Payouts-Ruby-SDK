bed8a851fea7e85f3df7fb61839205566d81d35a
Cheng Chang <chengchang@paypal.com>
2021-04-29 12:15:51 -0700

    added console output for status code

--- spec/payouts/payouts_post_spec.rb
+++ spec/payouts/payouts_post_spec.rb
@@ -12,6 +12,7 @@ describe PayoutsPostRequest do
     expect(resp.result).not_to be_nil
 
     responseBody = resp.result
+    put "Status Code: " + responseBody.status_code
     expect(responseBody.batch_header.payout_batch_id).not_to be_nil
     expect(responseBody.batch_header.batch_status).not_to be_nil
     expect(responseBody.batch_header.sender_batch_header.email_subject).to eq('This is a test transaction from SDK')
